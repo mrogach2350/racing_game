f16d535c9ef1cc0388ce6826144ec1689026c9ee
mrogach2350 <m.rogach@gmail.com>
2016-08-29 11:38:23 -0700

    adds two player functionality

--- js/app.js
+++ js/app.js
@@ -4,68 +4,81 @@ function Player(img, startBox) {
   this.img = img;
   this.box = startBox || '#p1start';
   this.availMoves = [];
-  this.moveThis = function (key){
-    // this.availMoves = whichMoves(this.box);
-    var id;
-    for (var i = 0; i < Object.keys(assignedKeys).length; i ++){
-      var keyArr = Object.keys(assignedKeys);
-      var keyId = "#" + i;
-      if (assignedKeys[keyId] === key){
-        console.log(keyArr[i]);
-        $(keyArr[i]).html(this.img);
-        this.box = keyArr[i];
-      }
-    }
-    // if (this.availMoves.indexOf(key) !== -1 ){
-    //   this.box = id;
-    //   $(id).html(this.img);
-    // }
-    fixBoard();
-  }
-//   this.move = function() {
-//     $(window).on('keydown', function handleEvent(event){
-//       var keyPressedCode = event.keyCode;
-//       if (keyPressedCode in keyCodes) {
-//         var keyPressed = keyCodes[keyPressedCode];
-//       }
-//       if (keyPressed in assignedKeys){
-//         console.log(assignedKeys[keyPressed]);
-//       }
-//     }
-//   }
+  // this.moveThis = function (key){
+  //   for (var i = 0; i < Object.keys(assignedKeys).length; i ++){
+  //     var keyArr = Object.keys(assignedKeys);
+  //     var keyId = "#" + i;
+  //     if (this.availMoves.indexOf(keyId) !== -1){
+  //       if (assignedKeys[keyId] === key){
+  //         $(keyArr[i]).html(this.img);
+  //         this.box = keyArr[i];
+  //       }
+  //     }
+  //   fixBoard();
+  //   }
+  // }
 }
 
-// function moveCar(){
-//
-// }
+function getAvailMoves(key){
+  for (var i = 0; i < Object.keys(assignedKeys).length; i ++){
+    var keyArr = Object.keys(assignedKeys);
+    var keyId = "#" + i;
+    if($(keyId).html() === player1.img){
+      player1.availMoves = whichMoves(keyId);
+    }
+    else if($('#p1Start').html() === player1.img){
+      player1.availMoves = whichMoves('#p1Start');
+    }
+    if($(keyId).html() === player2.img){
+      player2.availMoves = whichMoves(keyId);
+    }
+    else if($('#p2Start').html() === player2.img){
+      player2.availMoves = whichMoves('#p2Start');
+    }
+  }
+}
+function moveCars(key){
+  for (var i = 0; i < Object.keys(assignedKeys).length; i ++){
+    var keyArr = Object.keys(assignedKeys);
+    var keyId = "#" + i;
+    if (player1.availMoves.indexOf(keyId) !== -1){
+      if (assignedKeys[keyId] === key){
+        $(player1.box).html('');
+        $(keyArr[i]).html('<img src="imgs/redCar.png" alt="" />');
+        player1.box = keyArr[i];
+      }
+    }
+    if (player2.availMoves.indexOf(keyId) !== -1){
+      if (assignedKeys[keyId] === key){
+        $(player2.box).html('');
+        $(keyArr[i]).html('<img src="imgs/racecar.jpg" alt="" />');
+        player2.box = keyArr[i];
+      }
+    }
+  }
+   fixBoard();
+}
 
 function fixBoard(){
   for (var i = 0; i < availCodes.length; i++){
     var id = "#" + i;
-    if (id !== player1.box && player2.box){
+    if (id !== player1.box && id !== player2.box){
       if (id in assignedKeys){
         $(id).html(assignedKeys[id]);
       }
     }
   }
-  var box1 = player1.box;
-  if (box1 !== '#p1Start'){
+  if (player1.box !== '#p1Start'){
     $('#p1Start').html('');
   }
-  var box2 = player2.box;
-  if (box2!== '#p1Start'){
+  if (player2.box !== '#p2Start'){
     $('#p2Start').html('');
   }
 }
 
 $(document).ready(function() {
   //intentionally removed 'var' to put players into the global scope.
-  player1 = new Player('<img src="imgs/redCar.png" alt="" />', '#p1start');
-  player2 = new Player('<img src="imgs/racecar.jpg" alt="" />', '#p2start');
 
-
-  player1.availMoves = whichMoves('#p1Start');
-  player2.availMoves = whichMoves('#p2Start');
 
   $('#start').on('click', function handleEvent(event){
     event.preventDefault();
@@ -76,8 +89,10 @@ $(document).ready(function() {
       // $(id).text(id);
     };
     $('#finishBox').text("␣");
-    $('#p1Start').html(player1.img);
-    $('#p2Start').html(player2.img);
+    $('#p1Start').html('<img src="imgs/redCar.png" alt="" />');
+    $('#p2Start').html('<img src="imgs/racecar.jpg" alt="" />');
+    player1 = new Player($('#p1Start').html(), '#p1Start');
+    player2 = new Player($('#p2Start').html(), '#p2Start');
   });
 
   $('#reset').on('click', function handleEvent(event){
@@ -91,8 +106,8 @@ $(document).ready(function() {
     if (keyPressedCode in keyCodes) {
       var keyPressed = keyCodes[keyPressedCode];
     }
-    player1.moveThis(keyPressed);
-    player2.moveThis(keyPressed);
+    getAvailMoves(keyPressed);
+    moveCars(keyPressed);
     if($('#finishBox').html() === player1.img){
       alert('Player 1 wins');
     }
